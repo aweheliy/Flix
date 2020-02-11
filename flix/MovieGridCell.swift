4f8a6af26c33905cfa16711c83575f758108c7a5
Abdiaziz Weheliye <aweheliy@ucsd.edu>
2020-02-11 11:53:43 -0800

    final part done

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Abdiaziz Weheliye on 2/10/20.
+//  Copyright © 2020 Abdiaziz Weheliye. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
